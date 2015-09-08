f7ffbcfac1b4d5c8784bbffedfeb3e768b5dd9f3
jbdesbas <jb.desbas@gmail.com>
2015-09-08 18:48:55 +0200

    Reorganisation code

--- stats_uicn.py
+++ stats_uicn.py
@@ -45,30 +45,11 @@ for shape in shapes:
 	listData.append(gpd.GeoDataFrame.from_file(path+'/'+shape))
 
 data=gpd.GeoDataFrame(pd.concat(listData,ignore_index=True))
-
+###Travail sur lot de donnees
 data=data[data["nb"] >= 0] #filtre des obs négative
 data=data[data.geometry.area < 5000000] #filtre des polygon sup a 5km2
 data["date_obs"]=pd.to_datetime(data["date_obs"]) #convertire la date
 data["annee"]=data["date_obs"].dt.year
-
-##### Zone d occurence #########
-#Un mcp par annee
-occurence('annee').to_file(path+'/out/occurence_an.shp')
-#Et un mcp pour la periode de ref
-subdata=gpd.GeoDataFrame(data[data["date_obs"].dt.year.isin(years)])#selection sur la periode de ref
-mcp_ref = subdata['geometry'].unary_union.convex_hull #mcp total sur la periode de ref
-mcp_ref = gpd.GeoSeries(mcp_ref)
-mcp_ref.to_file(path+'/out/occurence_ref.shp')
-
-##### Zone d occupation #####
-occupation('annee').to_file(path+'/out/occup_an.shp')
-#Caclule de la zone pour la periode de ref
-data_centroid_ref=data_centroid[data_centroid["date_obs"].dt.year.isin(years)]
-occup_ref=sjoin(maillage[['ID','geometry']],data_centroid_ref[['geometry']])
-occup_ref=jointure2.drop_duplicates(['geometry'])
-occup_ref.to_file(path+'/out/occupation.shp')
-
-######Variation
 ##Definition des periodes et mise a jour du data
 if len(years)%2 == 0: #Paire
 	pos=len(years)/2
@@ -82,12 +63,29 @@ data.loc[data["annee"].isin(periode2),'periode']='P2'
 data.loc[data["annee"]<min(years),'periode']='Ant' #pour les anterierus
 data.loc[data["annee"]>max(years),'periode']='Post' #Trois pour les posterieur
 
-##Variation zone d occupation
-occup_per=occupation('periode')
-occup_per.to_file(path+'/out/occup_per.shp')
-##Variation zone d occurence
+##### Zone d occurence #########
+#Un mcp par annee
+mcp_an=occurence('annee')
+mcp_an.to_file(path+'/out/occurence_an.shp')
 mcp_per=occurence('periode')
 mcp_per.to_file(path+'/out/occurence_per.shp')
+#Et un mcp pour la periode de ref
+subdata=gpd.GeoDataFrame(data[data["date_obs"].dt.year.isin(years)])#selection sur la periode de ref
+mcp_ref = subdata['geometry'].unary_union.convex_hull #mcp total sur la periode de ref
+mcp_ref = gpd.GeoSeries(mcp_ref)
+mcp_ref.to_file(path+'/out/occurence_ref.shp')
+
+##### Zone d occupation #####
+##Par annne et par periode
+occup_an=occupation('annee')
+occup_an.to_file(path+'/out/occup_an.shp')
+occup_per=occupation('periode')
+occup_per.to_file(path+'/out/occup_per.shp')
+#Caclule de la zone pour la periode de ref
+data_centroid_ref=data_centroid[data_centroid["date_obs"].dt.year.isin(years)]
+occup_ref=sjoin(maillage[['ID','geometry']],data_centroid_ref[['geometry']])
+occup_ref=jointure2.drop_duplicates(['geometry'])
+occup_ref.to_file(path+'/out/occupation.shp')
 
 ##### Stats #####
 ref=pd.DataFrame()
